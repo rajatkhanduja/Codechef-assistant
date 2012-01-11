1d0a55cc3a4151634e6033152a8ee96282e8445e
Rajat Khanduja <rajatkhanduja13@gmail.com>
2012-01-11 21:47:44 +0530

    Made changes to make the code cross-platform

--- Codechef_assistant/codechef_assistant.js
+++ Codechef_assistant/codechef_assistant.js
@@ -1,3 +1,4 @@
+$(function() { 
 var img_url = chrome.extension.getURL("tick.jpg");
 // Check if the user is logged in
 var login_text = jQuery('div.login-user.login-name').text();
@@ -8,19 +9,19 @@ if ( login_text == null)
   exit(0);
 }
 
-var user_link = jQuery('div.login-user.login-name a').attr('href');    // This link contains the necessary information.
+var user_link = jQuery('div.login-user a').attr('href');    // This link contains the necessary information.
 // Get the list of solved problems in an array.
-var parser = new DOMParser();
+
 var t = jQuery.ajax(
   {
     url : user_link,
     datatype : "html",
     success : function (data) {
-      htmlDoc = parser.parseFromString(data,"text/xml");
       var i = 0 ; 
       links_array = new Array ();
-      $(htmlDoc).find('td a').each (function(){
-        if ($(this).attr('href').indexOf('status') != -1)
+      $(data).find('td a').each (function(){
+      	link = $(this).attr('href');
+        if (link.indexOf('status') != -1)
         {
           links_array[i++] = "/problems/" + $(this).attr('href').split(',')[0].match(/[a-zA-Z0-9]+/g)[1];
         }
@@ -37,3 +38,4 @@ var t = jQuery.ajax(
       });
     }
   });
+})
